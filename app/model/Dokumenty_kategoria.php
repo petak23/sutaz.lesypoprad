a66ca5863185b05f0c6b62e7523d068540f27d24
petak23 <petak23@gmail.com>
2017-04-03 09:25:59 +0200

    Verzia 0.1.4 - Pridaná DB tabuľka dokumenty_kategoria.

---
+++ app/model/Dokumenty_kategoria.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DbTable;
+
+/**
+ * Model starající se o tabulku dokumenty_kategoria
+ * Posledna zmena(last change): 03.04.2017
+ * 
+ * @author     Ing. Peter VOJTECH ml. <petak23@gmail.com>
+ * @copyright  Copyright (c) 2012 - 2017 Ing. Peter VOJTECH ml.
+ * @license
+ * @link       http://petak23.echo-msz.eu
+ * @version    1.0.0
+ */
+class Dokumenty_kategoria extends Table {
+  /** @var string */
+  protected $tableName = 'dokumenty_kategoria';
+}
