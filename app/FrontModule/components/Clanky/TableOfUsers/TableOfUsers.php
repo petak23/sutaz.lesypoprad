47e64285b5eb29c8ba99ac3b0bf0342da243e138
petak23 <petak23@gmail.com>
2017-05-03 07:29:53 +0200

    Verzia 0.3.2 - Odstránenie chyby vo výpise užívateľov.

--- app/FrontModule/components/Clanky/TableOfUsers/TableOfUsers.php
+++ app/FrontModule/components/Clanky/TableOfUsers/TableOfUsers.php
@@ -7,13 +7,13 @@ use DbTable;
 
 /**
  * Komponenta pre zobrazenie aktívnych uzivatelov pre FRONT modul
- * Posledna zmena(last change): 24.04.2017
+ * Posledna zmena(last change): 03.05.2017
  *
  * @author Ing. Peter VOJTECH ml <petak23@gmail.com>
  * @copyright Copyright (c) 2012 - 2017 Ing. Peter VOJTECH ml.
  * @license
  * @link http://petak23.echo-msz.eu
- * @version 1.0.2
+ * @version 1.0.3
  *
  */
 class TableOfUsersControl extends UI\Control {
@@ -22,6 +22,8 @@ class TableOfUsersControl extends UI\Control {
   private $dokumenty;
   /** @var DbTable\User_profiles */
   private $user_profiles;
+  /** @var DbTable\User_team */
+  private $user_team;
   /** @var Nette\Security\User */
   private $user;
   /** @var array - texty pre sablonu v inom jazyku */
@@ -33,10 +35,11 @@ class TableOfUsersControl extends UI\Control {
 
   /**
    * @param DbTable\Dokumenty $dokumenty */
-  public function __construct(DbTable\Dokumenty $dokumenty, DbTable\User_profiles $user_profiles, User $user) {
+  public function __construct(DbTable\Dokumenty $dokumenty, DbTable\User_profiles $user_profiles, DbTable\User_team $user_team, User $user) {
     parent::__construct();
     $this->dokumenty = $dokumenty;
     $this->user_profiles = $user_profiles;
+    $this->user_team = $user_team;
     $this->user = $user;
   }
 	
@@ -53,14 +56,16 @@ class TableOfUsersControl extends UI\Control {
    * @see Nette\Application\Control#render() */
   public function render() { 
     $this->template->setFile(__DIR__ . "/TableOfUsers.latte");
-    $pokus = $this->dokumenty->prispevky();
+    $prispevky = $this->dokumenty->prispevky();
     $tou = [];
-    foreach ($pokus as $p) {
-      $tou[] = ['id'    => $this->user->getIdentity()->id_registracia > 3 ? $p->id_user_profiles : 0,
-                'meno'  => $p->meno . " " . $p->priezvisko,
-                'tim'   => ($p->id_user_team !== NULL) ? $this->user_profiles->findOneBy(['id_user_team'=>$p->id_user_team])->user_team->nazov : "-",
-                'pocet' => $p->pocet,
-             ];
+    if ($prispevky !== FALSE && count($prispevky)) {
+      foreach ($prispevky as $p) {
+        $tou[] = ['id'    => ($this->user->isLoggedIn() && $this->user->getIdentity()->id_registracia > 3) ? $p->id_user_profiles : 0,
+                  'meno'  => $p->meno . " " . $p->priezvisko,
+                  'tim'   => ($p->id_user_team !== NULL) ? $this->user_team->teamName($p->id_user_team) : "-",
+                  'pocet' => $p->pocet,
+               ];
+      }
     }
     $this->template->tou = $tou;
     $this->template->sutaziacich = $this->user_profiles->findBy(["id_registracia"=>1])->count();
--- app/model/User_team.php
+++ app/model/User_team.php
@@ -4,15 +4,27 @@ namespace DbTable;
 
 /**
  * Model, ktory sa stara o tabulku user_team
- * Posledna zmena 31.03.2017
+ * Posledna zmena 03.05.2017
  * 
  * @author     Ing. Peter VOJTECH ml. <petak23@gmail.com>
  * @copyright  Copyright (c) 2012 - 2017 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version    1.0.0
+ * @version    1.0.1
  */
 class User_team extends Table {
   /** @var string */
   protected $tableName = 'user_team';
+  
+  /**
+   * Zisti nazov teamu
+   * @param int $id
+   * @return string */
+  public function teamName($id) {
+    if (($team = $this->find($id)) !== FALSE) {
+      return strlen($team->nazov) ? $team->nazov : "-";
+    } else {
+      return "-";
+    }
+  }
 }
